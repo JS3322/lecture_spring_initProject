6ccf0ab787ec8f60a0690ff42d82b25fff94990c
js3322 <js@cleancode.kr>
2022-08-07 03:52:34 +0900

    update

--- src/main/java/com/example/initproject/domain/Member.java
+++ src/main/java/com/example/initproject/domain/Member.java
@@ -1,15 +1,19 @@
 package com.example.initproject.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import javax.persistence.*;
+
+@ToString
+@Setter
+@Getter
 @Entity
 public class Member {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private long id;
     private String name;
---
+++ src/main/java/com/example/initproject/repository/JpaMemberRepository.java
@@ -0,0 +1,41 @@
+package com.example.initproject.repository;
+
+import com.example.initproject.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository {
+
+    //Springboot는 EntityManager를 자동으로 생성 (JPA라이브러리 다운 받는 시점에)
+    //JPA는 EntityManager로 인하여 동작하며, 주입받아야 함
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = em.createQuery("select m from Member m where m.name = ").setParameter("name", name).getResultList();
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
